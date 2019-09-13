99ecdb84a6974c22119ee3ce5cf32c35e1c59cc9
CzBiX <gliuwr@gmail.com>
2019-09-14 03:19:35 +0800

    Remove unused service worker

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import store from './store';
 import './filters';
 import './directives';
 import App from './App.vue';
-// import './registerServiceWorker';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
