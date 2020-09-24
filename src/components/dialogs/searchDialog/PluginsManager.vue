89938adafab1b0803fe5f70e7be75cee44ff838f
Bogdan Bogdanov <bbogdanov@vmware.com>
2020-09-24 09:42:57 +0300

    Add search plugin manager foundation (#58)

---
+++ src/components/dialogs/searchDialog/PluginsManager.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog
+    v-model="searchEngineState.isPluginManagerOpen"
+    max-width="20rem"
+    scrollable
+  >
+    <v-card>
+      <v-card-title> <v-icon>mdi-toy-brick</v-icon> Plugins manager </v-card-title>
+      <v-card-text>
+        <v-switch
+          v-for="(plugin, key) in searchEngineState.searchPlugins"
+          :key="key"
+          :input-value="plugin.enabled"
+          :label="plugin.fullName"
+          @change="togglePluginAvailability(plugin)"
+        />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { SearchEnginePage } from "@/store/types";
+import { SearchPlugin } from '@/types';
+import Vue from "vue";
+import Component from "vue-class-component";
+import { mapActions, mapState } from "vuex";
+
+@Component({
+  computed: {
+    ...mapState({
+      searchEngineState: "searchEngine"
+    })
+  },
+  methods: {
+    ...mapActions({
+      togglePluginAvailabilityAction: "togglePluginAvailability"
+    })
+  }
+})
+export default class PluginsManager extends Vue {
+  searchEngineState!: SearchEnginePage;
+  togglePluginAvailabilityAction!: (_: any) => void;
+
+  togglePluginAvailability(plugin: SearchPlugin) {
+    this.togglePluginAvailabilityAction(plugin);
+  }
+}
+</script>
---
+++ src/store/searchEngine.ts
@@ -0,0 +1,65 @@
+import { Module } from "vuex";
+import { SearchPlugin } from "@/types";
+import { SearchEnginePage } from "./types";
+import api from "@/Api";
+
+export default {
+  state: {
+    searchPlugins: [],
+    isPluginManagerOpen: false
+  },
+  mutations: {
+    setSearchPlugins(state, plugins: SearchPlugin[] | undefined | null) {
+      state.searchPlugins = plugins;
+    },
+    openPluginManager(state) {
+      state.isPluginManagerOpen = true;
+    },
+    closePluginManager(state) {
+      state.isPluginManagerOpen = false;
+    }
+  },
+  getters: {
+    allSearchPlugins(state): SearchPlugin[] | undefined | null {
+      return state.searchPlugins;
+    }
+  },
+  actions: {
+    fetchSearchPlugins({ dispatch }) {
+      // semantic helper
+      dispatch("getSearchPluginsRequest");
+    },
+    async getSearchPluginsRequest({ dispatch }) {
+      try {
+        const searchPlugins = await api.getSearchPlugins();
+
+        dispatch("getSearchPluginRequestSuccess", searchPlugins);
+      } catch {
+        dispatch("getSearchPluginsRequestFailure");
+      }
+    },
+    getSearchPluginRequestSuccess({ commit }, searchPlugins) {
+      commit("setSearchPlugins", undefined);
+
+      commit("setSearchPlugins", searchPlugins);
+    },
+    getSearchPluginRequestFailure({ commit }) {
+      commit("setSearchPlugins", null);
+    },
+    togglePluginAvailability({ dispatch }, plugin) {
+      dispatch("togglePluginEnableRequest", plugin);
+    },
+    async togglePluginEnableRequest({ dispatch }, plugin: SearchPlugin) {
+      try {
+        await api.enablePlugin(plugin, !plugin.enabled); // switch plugin enable state
+
+        dispatch("enablePluginRequestSuccess", plugin);
+      } catch {
+        // Do nothing
+      }
+    },
+    enablePluginRequestSuccess({ dispatch }) {
+      dispatch('fetchSearchPlugins'); // refresh the plugins
+    }
+  }
+} as Module<SearchEnginePage, any>;
